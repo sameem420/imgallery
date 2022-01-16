99d700775fb528cdc4c3aa3d1f230ce86e9bfc47
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2022-01-16 06:29:54 +0500

    :white_check_mark: fixed the key issue

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
+const key =
+  process.env.REACT_APP_CLIENT_ID ||
+  "Client-ID OuCRmZ8EgK5v94qbkaawBc7wVj3mOlmGqfsdwYFAO-U";
+
 export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization: process.env.REACT_APP_CLIENT_ID,
+    Authorization: key,
   },
 });
